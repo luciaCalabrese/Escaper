324e5e0e6a964e6bc18a42015eae5ff58e378b2d
Lucia <Lucia@gmail.com>
2021-10-07 10:27:44 +0200

    Paso de pantallas

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val boton1 = findViewById<Button>(R.id.bton)
 
+        boton1.setOnClickListener {
+
+            val cambiando = Intent(this, MainActivity2::class.java)
+
+            startActivity(cambiando)
+
+        }
     }
 }
