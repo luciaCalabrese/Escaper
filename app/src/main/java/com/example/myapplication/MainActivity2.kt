364c2bd92ad6c4d72917a5a07391c341d886c6cf
Lucia <Lucia@gmail.com>
2021-11-04 10:31:37 +0100

    Commit

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val botonfacil =  findViewById<Button>(R.id.button)
+
+        botonfacil.setOnClickListener {
+
+            val cambiando = Intent(this, facil::class.java)
+
+            startActivity(cambiando)
+
+        }
     }
+
+
 }
